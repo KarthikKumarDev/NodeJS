5c60d26a7fdd0fc6202d3945b0efe88a56905852
karthikkumar1996 <jkkr.1996@gmail.com>
2018-11-28 20:28:29 +0530

    feat: Delete User

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -95,23 +95,90 @@ handlers._users.get = function (data, callback) {
                 delete data.hashedPassword;
                 callback(200, data);
             } else {
-                callback(404, { 'Error': 'User not exists' });
+                callback(404, { 'Error': 'User does not exists' });
             }
         })
     } else {
         callback(400, { 'Error': 'Missing the required field' });
     }
-
 };
 
 // Users - Put
+// Required data: phone
+// Optional data: firstName, lastName, password (atleast one)
+// @TODO: User should be able to update only his object
 handlers._users.put = function (data, callback) {
+    var phone = typeof (data.payload.phone) === 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
 
+    // Check for the optional data
+    var firstName = typeof (data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    var lastName = typeof (data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    var password = typeof (data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            _data.read('users', phone, function (err, userData) {
+                if (!err && userData) {
+
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+
+                    if (password) {
+                        userData.password = helpers.hash(password);
+                    }
+
+                    // Update the User
+                    _data.update('users', phone, userData, function (err) {
+                        if (!err && data) {
+                            callback(200);
+                        } else {
+                            callback(500, { 'Error': 'Unable to update the User' });
+                        }
+                    });
+                } else {
+                    callback(404, { 'Error': 'User does not exists' });
+                }
+            })
+        } else {
+            callback(400, { 'Error': 'No data to update' });
+        }
+    } else {
+        callback(400, { 'Error': 'Missing the required field1' });
+    }
 };
 
 // Users - Delete
+// Required data: phone
+// optional data: none
+// @TODO: Only let Authenticated User to delete
 handlers._users.delete = function (data, callback) {
 
+    // Check the validity of phone number
+    var phone = typeof (data.queryStringObject.phone) === 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+    if (phone) {
+        // Lookup the User
+        _data.read('users', phone, function (err, data) {
+            if (!err && data) {
+                _data.delete('users', phone, function (err) {
+                    if (!err) {
+                        callback(200);
+                    } else {
+                        callback(500, { 'Error': 'Could not delete the User' })
+                    }
+                });
+            }
+            else {
+                callback(400, { 'Error': 'User does not exists' });
+            }
+        })
+    } else {
+        callback(400, { 'Error': 'Missing the required field' });
+    }
 };
 
 module.exports = handlers
